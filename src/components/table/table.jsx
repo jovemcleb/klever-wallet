0de7b954694a3de5afb455d3d032bc227cbcfbdc
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-08-21 22:50:39 -0300

    :lipstick:feat: adiciona sass e estilização da home

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './table.scss';
 
-export default function Table() {
+export default function Table(props) {
   return (
     <table>
       <thead>
         <tr>
-          <th>Tokens</th>
-          <th>Balance</th>
+          <th className="thead-token">Tokens</th>
+          <th className="thead-balance">Balance</th>
         </tr>
       </thead>
       <tbody></tbody>
--- src/components/wallet/wallet.jsx
+++ src/components/wallet/wallet.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import shootingStar from '../../images/shooting-star.svg';
 import Table from '../table/table';
+import './wallet.scss';
 
 export default function Wallet() {
   return (
     <main>
-      <img src={shootingStar} alt="Shooting star" width="50px" />
-      <button>Add Token</button>
+      <section className="bar-wallet">
+        <div>
+          <img className="shooting-star" src={shootingStar} alt="Shooting star" width="50px" />
+          <h1>Wish Wallet</h1>
+        </div>
+        <button className="add-token-button">Add Token</button>
+      </section>
       <Table />
     </main>
   );
